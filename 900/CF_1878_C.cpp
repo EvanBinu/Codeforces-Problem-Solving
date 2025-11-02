db366880028a51232cdbb6632c57928dedb42c4f
Evan Binu <evanbinu17@gmail.com>
2025-11-02 08:30:53 +0530

    1878_C

---
+++ 900/CF_1878_C.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <set>
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n,k,x;
+        cin >> n >> k >> x;
+        long long minsum = (k*(k+1)/2);
+        long long maxsum = (n*(n+1))/2-((n-k)*(n-k+1))/2;
+
+        if(x>=minsum && x<=maxsum ){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
